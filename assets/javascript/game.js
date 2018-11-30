d6b12b37c1e4c811b721628077bf014042c54ab4
SPallai <sangeeta.sumair@gmail.com>
2018-11-29 17:10:44 -0800

    updated game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,14 @@
 // global variables
 //____________________________________________________________________________
 // game counter
-var Wins = 0;
-var Losses = 0;
+var wins = 0;
+var losses = 0;
 var totalScore = 0;
 var StartNum = "";
+var crystal1 = Math.floor(Math.random() * 10) +1;
+var crystal2 = Math.floor(Math.random() * 10) +1;
+var crystal3 = Math.floor(Math.random() * 10) +1;
+var crystal4 = Math.floor(Math.random() * 10) +1;
 
 // functions
 //____________________________________________________________________________
@@ -13,9 +17,61 @@ var randomStartNum = Math.floor(Math.random() * 102) +19;
 
 StartNum = randomStartNum;
 
-$("#startnum").html(StartNum);
+function myFunction() {
+    StartNum = Math.floor(Math.random() * 102) +19;
+    $("#startnum").html(StartNum);
+}
 
-console.log();
+function Counter() {
+    if (StartNum === totalScore) {
+        wins++;
+        $("#winCounter").html(wins);
+        totalScore = 0;
+        myFunction();
+    } 
+    else if (totalScore > StartNum) {
+        losses++;
+        $("#lossCounter").html(losses);
+        totalScore = 0;
+        myFunction();
+    }
+}
+
+
+
+$(document).ready(function(){
+
+    $("#startnum").html(StartNum);
+
+    $("#crystal1").on("click", function() {
+        totalScore += crystal1;
+        $("#score").text(totalScore);
+        Counter();
+    });
+    $("#crystal2").on("click", function() {
+        totalScore += crystal2;
+        $("#score").text(totalScore);
+        console.log ("test)")
+        Counter();
+        
+    });
+    $("#crystal3").on("click", function() {
+        totalScore += crystal3;
+        $("#score").text(totalScore);
+        Counter();
+    });
+    $("#crystal4").on("click", function() {
+        totalScore += crystal4;
+        $("#score").text(totalScore);
+        Counter();
+    });
+
+    
+
+
+
+
+});
 // main process
 //____________________________________________________________________________
 
